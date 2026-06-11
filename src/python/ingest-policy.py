20b190f96703b6dd32b054bbcd19ef1e31253763
Cameron Geiser <cameron.geiser@gmail.com>
2026-06-11 14:01:40 -0700

    ingest policy complete, hookup to Chroma cloud complete.  uv setup

---
+++ src/python/ingest-policy.py
@@ -0,0 +1,68 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+import chromadb
+import uuid
+
+# client = chromadb.Client()
+# client = chromadb.PersistentClient(path="./chroma_data")
+# client = chromadb.HttpClient(host="localhost", port=8000)
+# client = chromadb.CloudClient()
+
+def uploadExpensePolicy(filename):
+    client = chromadb.CloudClient(
+        api_key=os.getenv("CHROMA_API_KEY"),
+        tenant=os.getenv("CHROMA_TENANT"),
+        database='dev'
+    )
+
+
+
+    collection = client.get_or_create_collection(name="policies")
+    try:
+        f = open(filename, "r", encoding="utf-8")
+        policies: list[str] = f.read().splitlines()
+    except FileNotFoundError:
+        print("File not Found")
+        return(False, "File not found")
+
+    print("Rows: ", len(policies))
+
+    collection.add(
+        ids=[str(uuid.uuid4()) for _ in policies],
+        documents=policies,
+        metadatas=[{"line":line} for line in range(len(policies))]
+    )
+    return(True, "success")
+
+
+def main():
+    print("Hello from expense-checker!")
+
+
+if __name__ == "__main__":
+    response = uploadExpensePolicy("policy2.txt")
+    print(response)
+
+
+# results = collection.query(
+#     query_texts= [
+#         "what is the return policy",
+#         "can I return customized items?"
+#     ],
+#     n_results = 5
+# )
+
+# print(results)
+
+
+# for i, results_enum in enumerate(results["documents"]):
+#     print(f"\n {i}")
+    # print("\n".join(results_enum))
+
+# vectorstore = Chroma.from_documents(chunks, embedding=OpenAIEmbeddings())
+
+
