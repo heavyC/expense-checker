102e6c1112739be2661017bccacc327c053290c7
Cameron Geiser <cameron.geiser@gmail.com>
2026-06-29 22:51:37 -0700

    Bake the env vars into the bundle at build time.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: 'standalone',
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL ?? '',
+  },
 };
 
 export default nextConfig;
